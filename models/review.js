92fb3b924a4f4e1e442b9101e89d340f5fb96146
rahul verma <48601181+rahulverma9449@users.noreply.github.com>
2023-11-22 08:43:15 +0530

    emplyoeee

---
+++ models/review.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+    review: {
+        type: String,
+    },
+    reviewer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    recipient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+}, {
+    timestamps: true,
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
